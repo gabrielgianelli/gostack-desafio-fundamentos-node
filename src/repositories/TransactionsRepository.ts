c4e1b944cfd5e41ebfaf442f5d8cb654fa828f27
Gabriel Gianelli <gabrielgianelli@gmail.com>
2020-07-19 00:47:31 -0300

    Refactoring method getBalance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -24,18 +24,26 @@ class TransactionsRepository {
   }
 
   public getBalance(): Balance {
-    const initialValue = 0;
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
 
-    const income = this.transactions.reduce(
-      (sum, transaction) =>
-        sum + (transaction.type === 'income' ? transaction.value : 0),
-      initialValue,
-    );
-
-    const outcome = this.transactions.reduce(
-      (sum, transaction) =>
-        sum + (transaction.type === 'outcome' ? transaction.value : 0),
-      initialValue,
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
     );
 
     return {
